7fc1da9d3e72971481696854820afc41bb7653a6
David Whitman <whitmadm@gmail.com>
2020-10-11 14:40:29 -0700

    Mod name, version, and author are now sourced from mod_info.json
    This enables better support for installing from file. Very few of this game's mods are on Nexus and the dominant way is to install without a mod manager.

--- game-starsector/index.js
+++ game-starsector/index.js
@@ -1,6 +1,16 @@
 // @ts-check
 const Promise = require('bluebird');
+const path = require('path');
+const rjson = require('relaxed-json');
+const { fs, log, util } = require('vortex-api');
 const winapi = require('winapi-bindings');
+
+const GAME_ID = 'starsector';
+const MOD_INFO_FILE = "mod_info.json"
+
+// Adapted from https://stackoverflow.com/a/24518413/1622788
+// Removes '#'s, which are used as comments by the game's json parser
+const COMMENT_STRIPPING_REGEX = /((["'])(?:\\[\s\S]|.)*?\2|\#(?![*\#])(?:\\.|\[(?:\\.|.)\]|.)*?\#)|\#.*?$|\#\*[\s\S]*?\*\#/gm;
 
 /**
  * @returns {string | Promise<String>}
@@ -20,12 +30,112 @@ function findGame() {
   }
 }
 
+function testSupportedContent(files, gameId) {
+  if (gameId !== GAME_ID) {
+    return Promise.resolve({ supported: false });
+  }
+
+  const contentPath = files.find(file => path.basename(file) === MOD_INFO_FILE);
+  return Promise.resolve({
+    supported: contentPath !== undefined,
+    requiredFiles: [contentPath],
+  });
+}
+
+async function installContent(files,
+  destinationPath,
+  gameId,
+  progressDelegate) {
+  const contentPath = files.find(file => path.basename(file) === MOD_INFO_FILE);
+  const basePath = path.dirname(contentPath);
+
+  let outputPath = basePath;
+
+  const contentFile = path.join(destinationPath, contentPath);
+  return fs.readFileAsync(contentFile, { encoding: 'utf8' }).then(data => {
+    const attrInstructions = [];
+    let parsed;
+    try {
+      // Strip '#' comments using regex, then parse using relaxed-json
+      parsed = rjson.parse(data.replace(COMMENT_STRIPPING_REGEX, "$1"));
+    } catch (err) {
+      log('warn', MOD_INFO_FILE + ' invalid: ' + err.message);
+      return Promise.resolve(attrInstructions);
+    }
+
+    // Function to get a value from mod_info.json by key
+    const getAttr = key => {
+      try {
+        return parsed[key];
+      } catch (err) {
+        log('info', 'attribute missing in ' + MOD_INFO_FILE, { key });
+        return "";
+      }
+    }
+
+    // If mod_info.json has no id, this is an invalid mod
+    const contentModId = getAttr('id');
+    if (contentModId === undefined) {
+      return Promise.reject(
+        new util.DataInvalid('invalid or unsupported ' + MOD_INFO_FILE));
+    }
+
+    outputPath = (contentPath.indexOf(MOD_INFO_FILE) > 0)
+      ? path.basename(path.dirname(contentPath))
+      : Promise.reject(
+        new util.DataInvalid('invalid or unsupported ' + MOD_INFO_FILE));
+
+    // Set the mod name based on mod_info.json
+    // TODO If the mod is being installed from Nexus, we don't want to overwrite the name
+    attrInstructions.push({
+      type: 'attribute',
+      key: 'customFileName',
+      value: getAttr('name').trim(),
+    });
+
+    // Set the mod version based on mod_info.json
+    attrInstructions.push({
+      type: 'attribute',
+      key: 'version',
+      value: getAttr('version').trim(),
+    });
+
+    // Description is fairly hidden in the UI, and we don't want to overwrite it
+    // if it's being set from Nexus Mods.
+    // attrInstructions.push({
+    //   type: 'attribute',
+    //   key: 'description',
+    //   value: getAttr('description').trim(),
+    // });
+
+    // Set the mod author based on mod_info.json
+    attrInstructions.push({
+      type: 'attribute',
+      key: 'author',
+      value: getAttr('author'),
+    });
+
+
+    return Promise.resolve(attrInstructions);
+  })
+    .then(attrInstructions => {
+      let instructions = attrInstructions.concat(files.filter(file =>
+        file.startsWith(basePath + path.sep) && !file.endsWith(path.sep))
+        .map(file => ({
+          type: 'copy',
+          source: file,
+          destination: path.join(outputPath, file.substring(basePath.length + 1))
+        })));
+      return { instructions };
+    });
+}
+
 /**
  * @param {import('vortex-api/lib/types/api').IExtensionContext} context
  */
 function main(context) {
   context.registerGame({
-    id: 'starsector',
+    id: GAME_ID,
     name: 'Starsector',
     mergeMods: true,
     queryPath: findGame,
@@ -36,6 +146,8 @@ function main(context) {
       'starsector.exe',
     ]
   });
+
+  context.registerInstaller('starsector', 50, testSupportedContent, installContent);
 
   return true;
 }
